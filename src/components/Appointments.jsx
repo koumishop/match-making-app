4de88b15156d85a719c441fa0643fd46456aa3e2
koumishop15 <koumishop@gmail.com>
2023-02-09 10:14:36 +0100

    refactor: reframe Appointment component

--- src/components/Appointments.jsx
+++ src/components/Appointments.jsx
@@ -4,24 +4,23 @@ import { Montserrat } from '@next/font/google';
 const montserrat = Montserrat({ subsets: ['latin'] });
 const meetPlace = "Salon Congo";
 
-export default function Appointments({ appointments }) {
+export default function Appointments({ appointment, company}) {
   return (
     <div className='w-full my-10'>
-    {appointments.userAppointment.rows.map((row)=>(
         <div className={`${montserrat.className} font-medium w-full text-secondary flex`}>
             <div className='p-3 flex border border-primary'>
                 <Icon icon="material-symbols:nest-clock-farsight-analog-outline-rounded" width={24} className='text-primary mr-2' />
-                {row.appointmentTime}
+                {appointment.appointmentTime}
             </div>
             <div className='p-3 flex border border-primary'>
                 <Icon icon="material-symbols:work-outline" width={24} className='text-primary mr-2' />
-                {appointments.company.companyType==='PUBLIC'?row.privateCompanyName:row.publicCompanyName}
+                {company.companyType==='PUBLIC'? privateCompanyName:appointment.privateCompanyName}
             </div>
             <div className='p-3 flex border border-primary'>
                 <Icon icon="ic:outline-place" width={24} className='text-primary mr-2' />
                 {meetPlace}
             </div>
-            {appointments.company.companyType==='PUBLIC'?
+            {company.companyType==='PUBLIC'?
             <div className='p-3 flex'>
                 <button type='button' className='w-1/7' onClick={()=>console.log("edit appointment")}><Icon icon="material-symbols:edit" width={24} className='text-primary' /></button>
             </div>
@@ -29,7 +28,6 @@ export default function Appointments({ appointments }) {
             <></>
             }                            
         </div>
-    ))}
     </div>
   );
 }
